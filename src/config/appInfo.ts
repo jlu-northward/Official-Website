de90ade597efea0cbb199006fb6098929a16be55
nohup <hi@nohup.life>
2025-11-16 16:31:56 +0800

    fix: update Google Play Store link and comment out alternative download button in AppHero component

--- src/config/appInfo.ts
+++ src/config/appInfo.ts
@@ -9,7 +9,7 @@ export const appInfo = {
 	},
 	storeLinks: {
 		apple: "https://apps.apple.com/cn/app/%E5%90%91%E5%8C%97app/id6753915149",
-		google: "https://a8jfgvfascumnxkt.public.blob.vercel-storage.com/%E5%90%91%E5%8C%97-release.apk",
+		google: "https://northward.nohup.life/northward-release.apk",
 		google2: "https://7rvcum8ituspxttc.public.blob.vercel-storage.com/android-builds/%E5%90%91%E5%8C%97-release.apk",
 	} as StoreLinks,
 };
